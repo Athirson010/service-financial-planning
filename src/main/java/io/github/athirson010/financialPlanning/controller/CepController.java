9d5abd114b4c64484ae8f9df05bb81ab05b312d1
Athirson010 <athirson.candido@bandtec.com.br>
2023-01-20 21:58:01 -0300

    add: CEP controller

--- src/main/java/io/github/athirson010/financialPlanning/controller/CepController.java
+++ src/main/java/io/github/athirson010/financialPlanning/controller/CepController.java
@@ -9,12 +9,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @Tag(name = "Buscar CEP")
+@RequestMapping(value = "/cep", produces = MediaType.APPLICATION_JSON_VALUE)
+@RestController
 public class CepController extends RestSecurity {
     @Autowired
     protected CepService service;
