70786a12163ab9eb687f8ac6eee0ef14535bec73
athirson.candido <athirson.candido@bandtec.com.br>
2023-02-18 09:50:46 -0300

    update: cors

--- src/main/java/io/github/athirson010/financialPlanning/config/CorsConfig.java
+++ src/main/java/io/github/athirson010/financialPlanning/config/CorsConfig.java
@@ -1,8 +1,7 @@
 package io.github.athirson010.financialPlanning.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,16 +9,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    private Logger logger = LoggerFactory.getLogger(CorsConfig.class);
+@Slf4j
+public class CorsConfig {
     @Autowired
-    private ApplicationContext context;
+    ApplicationContext context;
 
     @Bean
+    public CorsWebFilter corsConfigurer() {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.addAllowedOrigin("*");
+        cors.addAllowedMethod("*");
+        cors.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cors);
+        return new CorsWebFilter(source);
+    }
+
+/*    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             public void addCorsMappings(CorsRegistry registry) {
@@ -27,12 +40,12 @@ public class CorsConfig implements WebMvcConfigurer {
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
             }
         };
-    }
+    }*/
 
     @Bean
     public CommandLineRunner runner() throws Exception {
         return args -> {
-            logger.info("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+            log.info("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
             System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
         };
     }
@@ -44,7 +57,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     public void execAntesInjecaoDependenciaTexto() {
         int banana = 1;
-        logger.info("ANTES DA INJEÇÃO!");
+        log.info("ANTES DA INJEÇÃO!");
         if (banana != 1) {
             closeApplication();
         }
